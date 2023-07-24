f62faa1694a9be6d86f19cff0b642d4b271650a4
Joseph Morales Silva <josephzx1@hotmail.com>
2023-07-24 17:36:35 -0600

    Create navigation model

---
+++ src/models/navigationItems.model.js
@@ -0,0 +1,14 @@
+export const NAVIGATION_ITEMS = {
+  HOME: "home",
+  WORK: "work",
+  SKILLS: "skills",
+  CONTACT: "contact",
+  ABOUT: "about",
+};
+export const NAVIGATION_ITEMS_LIST = [
+  NAVIGATION_ITEMS.HOME,
+  NAVIGATION_ITEMS.ABOUT,
+  NAVIGATION_ITEMS.WORK,
+  NAVIGATION_ITEMS.CONTACT,
+  NAVIGATION_ITEMS.SKILLS,
+];
