e84673d83b8c3fbf2f7cadf473e2d8dd2c68b0e5
Joseph Morales Silva <josephzx1@hotmail.com>
2023-07-24 17:36:50 -0600

    Create hoc app wrapper

---
+++ src/wrapper/AppWrap.wrapper.jsx
@@ -0,0 +1,18 @@
+import { NavigationDots, SocialMedia } from "../components";
+
+export const AppWrap = ({ Component, idName, classNames = "" }) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app_flex">
+          <Component />
+          <div className="copyright">
+            <p className="p-text">@2023 Joseph</p>
+            <p className="p-text">All rights reserved</p>
+          </div>
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
