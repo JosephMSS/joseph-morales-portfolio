5258fbd197173dd6e93d70eb78476b3979b8a6dd
Joseph Morales Silva <josephzx1@hotmail.com>
2023-07-24 16:07:17 -0600

    Format about component code

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -24,27 +24,28 @@ export const About = () => {
       </h2>
 
       <div className="app__profiles">
-        {aboutData.length && aboutData.map((about, index) => {
-          return (
-            <motion.div
-              whileInView={{
-                opacity: 1,
-              }}
-              whileHover={{ scale: 1.1 }}
-              transition={{ duration: 0.5, type: "tween" }}
-              className="app__profile-item"
-              key={`${about.title}-${index}`}
-            >
-              <img src={about.imgUrl} alt={about.title} />
-              <h2 className="bold-text" style={{ marginTop: 20 }}>
-                {about.title}
-              </h2>
-              <p className="p-text" style={{ marginTop: 10 }}>
-                {about.description}
-              </p>
-            </motion.div>
-          );
-        })}
+        {aboutData.length &&
+          aboutData.map((about, index) => {
+            return (
+              <motion.div
+                whileInView={{
+                  opacity: 1,
+                }}
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.5, type: "tween" }}
+                className="app__profile-item"
+                key={`${about.title}-${index}`}
+              >
+                <img src={about.imgUrl} alt={about.title} />
+                <h2 className="bold-text" style={{ marginTop: 20 }}>
+                  {about.title}
+                </h2>
+                <p className="p-text" style={{ marginTop: 10 }}>
+                  {about.description}
+                </p>
+              </motion.div>
+            );
+          })}
       </div>
     </>
   );
