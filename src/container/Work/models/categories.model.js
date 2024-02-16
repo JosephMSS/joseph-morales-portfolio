7ad2d6a15c371e1a754ecf27a32c2d707d72abd3
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-15 20:38:35 -0600

    Remove unused category constants

--- src/container/Work/models/categories.model.js
+++ src/container/Work/models/categories.model.js
@@ -1,12 +1,3 @@
 export const CATEGORIES = {
-  EXPRESS: "Express JS",
-  REACT: "React JS",
-  NEST: "Nest JS",
   ALL: "All",
 };
-export const CATEGORY_LIST = [
-  CATEGORIES.ALL,
-  CATEGORIES.EXPRESS,
-  CATEGORIES.NEST,
-  CATEGORIES.REACT,
-];
