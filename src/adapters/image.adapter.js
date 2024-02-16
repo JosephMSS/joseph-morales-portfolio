ee40a360cc05a623ed366306a3833322b71d7606
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-15 20:36:27 -0600

    Add image adapter module

---
+++ src/adapters/image.adapter.js
@@ -0,0 +1,9 @@
+import imageUrlBuilder from "@sanity/image-url";
+import { client } from "../libs/httpClient/sanity.lib";
+const builder = imageUrlBuilder(client);
+
+export function imageAdapter(url) {
+  const adapter = builder.image(url);
+  console.log("🚀 ~ imageAdapter ~ adapter:", adapter);
+  return adapter;
+}
