557e3fbbdd26e36ed830c375c2bdcc344393bfd8
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-15 20:37:17 -0600

    Refactor workDataAdapter function to improve data adaptation

--- src/container/Work/adapters/work.adapter.js
+++ src/container/Work/adapters/work.adapter.js
@@ -1,12 +1,14 @@
+import { imageAdapter } from "../../../adapters/image.adapter";
+
 export const workDataAdapter = (data) => {
   const adaptData = data;
   const response = adaptData.map((data) => ({
     name: data.title,
     description: data.description,
-    tags: data?.tags,
-    projectLink: data?.projectLink,
-    codeLink: data?.codeLink,
-    imgUrl: data?.imageUrl,
+    tags: data?.tags.map((category) => category.name),
+    projectLink: data?.projectLink || null,
+    codeLink: data?.codeLink || null,
+    imgUrl: data.imgUrl ? imageAdapter(data.imgUrl).auto("format").url() : "",
   }));
   return response;
 };
