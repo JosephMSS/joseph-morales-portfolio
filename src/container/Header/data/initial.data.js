9e716c2a3cbd2958373ab37670f36a02bea50e70
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-18 14:31:39 -0600

    Update initial profile data

--- src/container/Header/data/initial.data.js
+++ src/container/Header/data/initial.data.js
@@ -1,10 +1,9 @@
-import { images } from "../../../constants";
 
 export const INITIAL_PROFILE = {
-  name: "Joseph Morales",
-  greeting: "Hello, I am",
-  greetingIcon: "👋",
-  mainPositions: ["Full stack Developer", "Freelancer"],
-  mainTechnologies: [images.react, images.node, images.typescript],
-  profileImage: images.profile,
+  name: "",
+  greeting: "",
+  greetingIcon: "",
+  mainPositions: [],
+  mainTechnologies: [],
+  profileImage: "",
 };
