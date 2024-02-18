a689142f2b8d34b146a808f7e9e88b279e7d663f
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-18 14:24:03 -0600

    Add initial data for header

---
+++ src/container/Header/data/initial.data.js
@@ -0,0 +1,10 @@
+import { images } from "../../../constants";
+
+export const INITIAL_PROFILE = {
+  name: "Joseph Morales",
+  greeting: "Hello, I am",
+  greetingIcon: "👋",
+  mainPositions: ["Full stack Developer", "Freelancer"],
+  mainTechnologies: [images.react, images.node, images.typescript],
+  profileImage: images.profile,
+};
