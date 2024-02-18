b131f325c9edab8abc6dae1eae901a1c46c29064
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-18 14:31:21 -0600

    Add profileAdapter function to handle profile data

---
+++ src/container/Header/adapters/profile.adapter.js
@@ -0,0 +1,16 @@
+import { imageAdapter } from "../../../adapters/image.adapter";
+
+export const profileAdapter = (data) => {
+  const profile = {
+    greeting: data.greeting,
+    name: data.name,
+    greetingIcon: data.greetingIcon,
+    mainPositions: data.mainPositions,
+    mainTechnologies: data.mainTechnologies.map((technology) =>
+      imageAdapter(technology).auto("format").url()
+    ),
+    profileImage: imageAdapter(data.profileImage).auto("format").url(),
+  };
+  console.log("🚀 ~ profileAdapter ~ profile:", profile);
+  return profile;
+};
