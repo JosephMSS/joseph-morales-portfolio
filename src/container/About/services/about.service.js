cf05ddfb7869d7f0d7fdd25daa284a94c2313629
Joseph Morales Silva <josephzx1@hotmail.com>
2024-02-22 19:25:02 -0600

    Refactor fetchAboutData to use findCertificatesService

--- src/container/About/services/about.service.js
+++ src/container/About/services/about.service.js
@@ -1,4 +1,13 @@
-import { data } from "../data/about.data";
-export const fetchAboutData = async () => {
-  return data;
+import { client } from "../../../libs/httpClient/sanity.lib";
+import { aboutDataAdapter } from "../adapters/about.adapter";
+const findCertificatesQuery = '*[_type == "educationCertificates"]';
+export const findCertificatesService = async ({ signal }) => {
+  const certificates = await client.fetch(
+    findCertificatesQuery,
+    {},
+    {
+      signal,
+    }
+  );
+  return aboutDataAdapter(certificates);
 };
